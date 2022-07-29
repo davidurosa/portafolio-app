b1b4beb1d103e4922077f65ea10d175fbcc780c1
davidurosa <urosaclawred21@gmail.com>
2022-07-29 07:13:46 -0400

    nuevo3

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -69,7 +69,7 @@ const Experience = () => {
           </div>
         </div>
         <div className="experiencie__backend">
-          <h3>backend developmen</h3>
+          <h3>Backend developmen</h3>
           <div className="experience__content">
             <article className="experience__details">
               <BsPatchCheckFill className="experience__details-icon"/>
